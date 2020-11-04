bf81b211072ada86c6cd0d928b1f9c76fc3812b9
Miguel Angel Loera Guerrero <301910560@ute.edu.mx>
2020-11-04 14:34:34 -0600

    Lo intente

---
+++ YouOpine/YouOpine.DataAccess/YouOpineDbContext.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.Entity;
+using Org.BouncyCastle.Asn1.X509.Qualified;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouOpine.Entities;
+
+namespace YouOpine.DataAccess
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class YouOpineDbContext : DbContext
+    {
+        public YouOpineDbContext()
+            : base("name=YouOpineDbContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Linea que sirve para eliminar el plural del nombre de las tablas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
