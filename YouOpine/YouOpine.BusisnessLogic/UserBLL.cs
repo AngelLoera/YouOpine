6a5a1c3d10cbb7919b7af42a065e2ba863bd6cdf
Miguel Angel Loera Guerrero <301910560@ute.edu.mx>
2020-11-05 17:06:22 -0600

    Reglas de Negocio y Acceso a Datos Min 28:36

---
+++ YouOpine/YouOpine.BusisnessLogic/UserBLL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouOpine.BusisnessLogic
+{
+    public class UserBLL
+    {
+        /// <summary>
+        /// Metodo para iniciar sesion en el aplicativo desarrollado
+        /// </summary>
+        /// <param name="username">Nombre del usuario</param>
+        /// <param name="password">Contraseña</param>
+        /// <returns>Mensaje</returns>
+        public static string LogInSystem(string username, string password)
+        {
+            //Esta variable me servira para poder enviar mensajes de retroalimentacion a la interfaz de usuario
+            string message = string.Empty;
+            //Escenario #1 (regla de Negocio)
+            /*
+             * Dado un usuario que se esta identificando en el sistema cuando
+             * el ingreso del usuario y contraseña son correctos entonces el
+             * sistema permitira el ingreso al menu principla
+             */
+            //Una vez que se mando llamar el metodo para iniciar sesion, valido el resultado obtenido
+            bool IsLogged = DataAccess.UserDAL.LogInSystem(username,password);
+            
+            //Si el resutlado es True, quiere decir que el usuario existe y puede iniciar sesion
+            //pero si el resultado es False, quiere decir que el usuario no existe y por lo tanto no puede iniciar sesion
+            if (IsLogged)
+            {
+                message = "Bienvenido a You Opine";
+            }
+            else
+            {
+                message = "Error al inicar en la aplicación, favor de intentarlo nuevamente";
+            }
+
+            return message;
+        }
+    }
+}
---
+++ YouOpine/YouOpine.DataAccess/UserDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouOpine.DataAccess
+{
+    public class UserDAL
+    {
+        /// <summary>
+        /// Metodo para iniciar sesion en el aplicativo desarrollado
+        /// </summary>
+        /// <param name="username">Nombre del usuario</param>
+        /// <param name="password">Contraseña</param>
+        /// <returns>Variable booleana con el resultado</returns>
+        public static bool LogInSystem(string username, string password)
+        {
+            //De manera inicial, mi variable para controlar el inicio de sesion se encuentra en false,
+            //cambiando de estado al validar las credenciales proporcionadas.
+            bool IsLogged = false;
+
+            //Metodo #1 para iniciar sesion utilizando LINQ
+
+            //El DbContext es utilizado como contenedor para conectarse a la base de datos
+            //anteriormente creada en MySql
+            using (YouOpineDbContext dbCtx = new YouOpineDbContext())
+            {
+                //TRANSAC-SQL --> LINQ
+                //Donde la instruccion SQL seria: SELECT * FROM User WHERE Username = '?' AND Password = '?'
+                //En LINQ se aplicaria asi: SELECT * FROM User en LINQ seria dbCtc.Users
+                //En el WHERE de SQL en LINQ seria .Where (condicion)
+                //SingleOrDefault -> Regresa el objeto o registro que encuentre con esas caracteristicas
+                //ToList() -> Un registro en forma de tabla con filas y columnas
+                //First() -> Regresa el primer objeto que coincida con las caracteristicas
+
+                var query = dbCtx.Users
+                    .Where(x => x.UserName == username && x.Password == password)
+                    .SingleOrDefault();
+
+                //Una vez que tu hiciste la consulta a la base de datos (QUERY), validamos el resultado
+                if (query != null)
+                    IsLogged = true; //Quiere decir que el usuario SI EXISTE en la base de datos
+            }
+
+            //Vamos a regresar el estado de inicio de sesion al final de l metodo
+            return IsLogged;
+        }
+    }
+}
