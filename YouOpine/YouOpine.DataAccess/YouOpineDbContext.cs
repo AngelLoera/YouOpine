51c4760ce3cdab8f28ae8bc4880e196e14f53d3b
AngelLoera <301910560@bis.ute.edu.mx>
2020-11-23 23:11:41 -0600

    Nose ni que

--- YouOpine/YouOpine.DataAccess/YouOpineDbContext.cs
+++ YouOpine/YouOpine.DataAccess/YouOpineDbContext.cs
@@ -1,6 +1,4 @@
-﻿using MySql.Data.Entity;
-using Org.BouncyCastle.Asn1.X509.Qualified;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
@@ -10,25 +8,25 @@ using System.Threading.Tasks;
 using YouOpine.Entities;
 
 namespace YouOpine.DataAccess
-{
-    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
-    public class YouOpineDbContext : DbContext
     {
-        public YouOpineDbContext()
-            : base("name=YouOpineDbContext")
+        [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+        public class YouOpineDbContext : DbContext
         {
+            public YouOpineDbContext()
+                : base("name=YouOpineDbContext")
+            {
 
-        }
+            }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
+            public DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //Linea que sirve para eliminar el plural del nombre de las tablas
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            {
+                //Linea que sirve para eliminar el plural del nombre de las tablas
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
-            base.OnModelCreating(modelBuilder);
+                base.OnModelCreating(modelBuilder);
+            }
+
         }
     }
-}
---
+++ YouOpine/YouOpine.Entities/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouOpine.Entities
+{
+    public class Usuario
+    {
+        [key]
+        public int ID_Us { get; set; }
+
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        [MaxLength(15, ErrorMessage = "Nombre de tener 20 caracteres o menos"), MinLength(5)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es requerido")]
+        [MaxLength(15, ErrorMessage = "Correo de tener 30 caracteres o menos"), MinLength(5)]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [MaxLength(10, ErrorMessage = "La contraseña debe tener 15 caracteres o menos"), MinLength(5)]
+        public string Contraseña { get; set; }
+
+    }
+}
